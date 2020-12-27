1059b22820a5ef53dbd2bdb112ef38d7f6b33112
FedericoStazi <stazi.federico@gmail.com>
2020-12-27 18:43:30 +0100

    Graph distance embedding

--- realworld_benchmark/data/molecules.py
+++ realworld_benchmark/data/molecules.py
@@ -87,7 +87,7 @@ class StructureAwareGraph(torch.utils.data.Dataset):
 
         #print(self.graph_labels[0])
 
-        #print()
+        print()
 
     def __len__(self):
         return self.n_samples
@@ -122,8 +122,7 @@ class MoleculeDataset(torch.utils.data.Dataset):
     def collate(self, samples):
         # The input samples is a list of pairs (graph, label).
         graphs, labels = map(list, zip(*samples))
-        labels = torch.cat(labels).float()
-        labels = torch.cuda.FloatTensor([[x] for x in labels])
+        labels = torch.tensor(np.array(labels)).unsqueeze(1)
         tab_sizes_n = [graphs[i].number_of_nodes() for i in range(len(graphs))]
         tab_snorm_n = [torch.FloatTensor(size, 1).fill_(1. / float(size)) for size in tab_sizes_n]
         snorm_n = torch.cat(tab_snorm_n).sqrt()
