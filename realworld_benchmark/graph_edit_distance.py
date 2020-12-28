90d98879f58a0ed6b0a6c80c26c79a9ef4247ca9
FedericoStazi <stazi.federico@gmail.com>
2020-12-28 10:39:53 +0100

    Graph distance embedding

--- realworld_benchmark/graph_edit_distance.py
+++ realworld_benchmark/graph_edit_distance.py
@@ -10,7 +10,7 @@ def graph_distance(a, b):
     return abs(a.number_of_nodes() - b.number_of_nodes())
 
 def embedding_distances(embeddings):
-    n = len(scores)
+    n = len(embeddings)
     a = embeddings.repeat(n, 1)
     b = embeddings.unsqueeze(1).repeat(1, n, 1).flatten(end_dim=1)
     return torch.sum((a - b) ** 2, dim=1)
